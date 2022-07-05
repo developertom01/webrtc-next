2d0e661999e896eedee14bde5e5a6c9bde5b68d4
Thomas Sarpong <53011231+developertom01@users.noreply.github.com>
2022-07-05 18:22:18 +0000

    Added routes

--- server/manager/expressManager.ts
+++ server/manager/expressManager.ts
@@ -4,19 +4,21 @@ import { AppPort } from "server/ports/app";
 import { FrameworkRightPort } from "server/ports/frameworkRight";
 import { parse } from "url";
 import WebRtcManager from "./WebrtcManager";
+import appRouter from "../adopters/framework/right/http/routers";
 export default class ExpressManager implements FrameworkRightPort<Express> {
   private _instance: Express;
 
   constructor(
     nextApp: NextServer,
     webRtcServer: WebRtcManager,
-    appAdopter: AppPort
+    apiAdopter: AppPort
   ) {
     this._instance = express();
     this._instance.use(express.urlencoded());
     this._instance.use(express.json());
     const handle = nextApp.getRequestHandler();
     this._instance.use("webrtc", webRtcServer.instance);
+    this._instance.use("/api", appRouter(apiAdopter));
     this._instance.use((req, res) => {
       handle(req, res, parse(req.url!, true));
     });
